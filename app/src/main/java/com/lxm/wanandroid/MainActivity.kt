5be7ecc474429cbbfc6efd8043433702bd2f39c6
liuxueming <bysir521@163.com>
2019-07-11 18:21:04 +0800

    update drawlayout

--- app/src/main/java/com/lxm/wanandroid/MainActivity.kt
+++ app/src/main/java/com/lxm/wanandroid/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.lxm.wanandroid
 
 import android.os.Bundle
-import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
@@ -23,10 +21,11 @@ import kotlinx.android.synthetic.main.main_content.*
 
 
 const val PHOTO_URL = "http://cdn.duitang.com/uploads/blog/201404/22/20140422142715_8GtUk.thumb.600_0.jpeg"
+
 class MainActivity : AppCompatActivity() {
 
     private val mTitles by lazy {
-        mutableListOf("主页","知识体系","导航","项目","福利")
+        mutableListOf("主页", "知识体系", "导航", "项目", "福利")
     }
     private val mFragments: MutableList<Fragment> by lazy {
         mutableListOf<Fragment>()
@@ -41,37 +40,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         StatusBarUtil.setColor(this, resources.getColor(R.color.colorTheme), 0)
         setSupportActionBar(toolbar)
-        toolbar.setNavigationIcon(R.drawable.photo)
-        val actionBar = supportActionBar
-        actionBar?.setDisplayShowTitleEnabled(false)
-
-        layout.run {
-            val toggle = ActionBarDrawerToggle(this@MainActivity, this,toolbar, R.string.navigation_open, R.string.navigation_close)
-            addDrawerListener(toggle)
-            toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        iv_photo.setOnClickListener {
+            drawerLayout.openDrawer(Gravity.START)
         }
-
         val imageView = navigationView.getHeaderView(0).findViewById<ImageView>(R.id.ivHeadView)
         val textView = navigationView.getHeaderView(0).findViewById<TextView>(R.id.tvUserName)
-
-        Glide.with(this@MainActivity)
-            .load(PHOTO_URL)
-            .transition(DrawableTransitionOptions.withCrossFade(500))
-            .transform(CircleCrop())
-            .into(imageView)
+        showPhoto(imageView)
+        showPhoto(iv_photo)
         navigationView.setNavigationItemSelectedListener {
             when (it.itemId) {
 
                 R.id.colletion -> {
                 }
-                R.id.about ->{
+                R.id.about -> {
 
                 }
                 else -> {
 
                 }
             }
-            layout.closeDrawer(Gravity.START)
+            drawerLayout.closeDrawer(Gravity.START)
             true
         }
 
@@ -87,8 +76,16 @@ class MainActivity : AppCompatActivity() {
         mAdapter.setFragments(mFragments)
         mAdapter.setTitles(mTitles)
         view_pager.adapter = mAdapter
-        view_pager.offscreenPageLimit=5
+        view_pager.offscreenPageLimit = 5
         tablayout.setViewPager(view_pager)
+    }
+
+    private fun showPhoto(imageView: ImageView) {
+        Glide.with(this@MainActivity)
+            .load(PHOTO_URL)
+            .transition(DrawableTransitionOptions.withCrossFade(500))
+            .transform(CircleCrop())
+            .into(iv_photo)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -98,21 +95,30 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
+        return when (item.itemId) {
             R.id.action_search -> {
 
-                return true
+                true
             }
-            else -> return super.onOptionsItemSelected(item)
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (drawerLayout.isDrawerOpen(Gravity.START)) {
+                drawerLayout.closeDrawer(Gravity.START)
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
+    }
 }
 
 class MyPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
 
     var fragmentList: MutableList<Fragment> = mutableListOf()
-    var titleList: MutableList<String>  = mutableListOf()
+    var titleList: MutableList<String> = mutableListOf()
 
     override fun getCount(): Int {
         return fragmentList.size
@@ -133,6 +139,5 @@ class MyPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fr
     fun setTitles(mTitles: MutableList<String>) {
         titleList = mTitles
     }
-
 }
 
